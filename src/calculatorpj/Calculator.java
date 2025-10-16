92aaeb147b757f5e68cdcdc902b7170d4c49f824
최정혁 <jeonghyeogc01@gmaile.com>
2025-10-16 16:40:13 +0900

    Calculator LV2 메서드 생성 및 결과값 저장 추가

--- src/calculatorpj/Calculator.java
+++ src/calculatorpj/Calculator.java
@@ -18,6 +18,30 @@ public class Calculator {
 
 
     // 기능
+    public  int calculate(int num1, int num2, char cs) {
+
+        int result = 0;
+        if (cs == '+') {
+            result = num1 + num2;
+        } else if (cs == '-') {
+            result = num1 - num2;
+        } else if (cs == '*') {
+            result = num1 * num2;
+        } else if (cs == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                System.out.println("0으로 나눌 수 없습니다.");
+                return 0;
+            }
+        } else {
+            System.out.println("잘못된 연산자입니다.");
+            return 0;
+        }
+        resultList.add(result);
+        return result;
+    }
+
 
 
 
