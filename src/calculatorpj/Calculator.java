8b43aa6f19dfed09ad32066e4f287dc2859d8de7
최정혁 <jeonghyeogc01@gmaile.com>
2025-10-16 15:47:15 +0900

    Calculator LV2 클래스 생성 및 컬렉션 타입설정

---
+++ src/calculatorpj/Calculator.java
@@ -0,0 +1,25 @@
+package calculatorpj;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    // 속성
+    List<Integer> resultList = new ArrayList<>();
+
+
+    // 생성자
+    Calculator() {
+
+    }
+
+
+
+    // 기능
+
+
+
+
+}
