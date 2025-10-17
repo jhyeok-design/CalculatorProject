ab96cf28652fc84db1a3edde7264b0ba62505cf9
최정혁 <jeonghyeogc01@gmaile.com>
2025-10-17 19:56:54 +0900

    Main LV2 정수 설정 try catch 수정

--- src/calculatorpj/Main.java
+++ src/calculatorpj/Main.java
@@ -1,6 +1,7 @@
 package calculatorpj;
 
 import java.util.InputMismatchException;
+
 import java.util.Scanner;
 
 public class Main {
@@ -10,43 +11,36 @@ public class Main {
 
         while (true) {
             int num1 = 0;
+            int num2 = 0;
             try {
                 System.out.println("첫번째 정수를 입력하세요.");
                 num1 = scanner.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("숫자를 입력하세요.");
-                scanner.nextLine();
-                continue;
-            }
 
-            System.out.println("사칙연산 기호를 입력하세요.");
-            char operator = scanner.next().charAt(0);
+                System.out.println("사칙연산 기호를 입력하세요.");
+                char operator = scanner.next().charAt(0);
 
-
-            int num2 = 0;
-            try {
                 System.out.println("두번째 정수를 입력하세요.");
                 num2 = scanner.nextInt();
+
+                Integer result = calculator.calculate(num1, num2, operator);
+
+                if (result != null) {
+                    System.out.println("결과 값 : " + result);
+                    System.out.println(calculator.getResultList());
+                    calculator.removeResult();
+                }
+                // 계산기 delete느낌으로 처음으로 돌아가도록 continue 설정
             } catch (InputMismatchException e) {
                 System.out.println("숫자를 입력하세요.");
                 scanner.nextLine();
                 continue;
             }
-            Integer result = calculator.calculate(num1, num2, operator);
-            if (result != null) {
-                System.out.println("결과 값 : " + result);
-                System.out.println(calculator.getResultList());
-                calculator.removeResult();
+
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                String answer = scanner.next();
+                  if (answer.equals("exit")) {
+                   break;
             }
-
-
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String answer = scanner.next();
-            if (answer.equals("exit")) {
-                break;
-            }
-
-
         }
     }
 }
