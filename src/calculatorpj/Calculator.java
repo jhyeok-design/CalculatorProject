c973c55ccec42c44627ee569c168a3fac43f1e02
최정혁 <jeonghyeogc01@gmaile.com>
2025-10-17 19:10:10 +0900

    Calculator LV2 조건문을 switch문으로 변경, 정수 범위값 메서드로 지정

--- src/calculatorpj/Calculator.java
+++ src/calculatorpj/Calculator.java
@@ -1,8 +1,8 @@
 package calculatorpj;
 
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+
 
 public class Calculator {
 
@@ -13,45 +13,61 @@ public class Calculator {
     // 생성자
     Calculator() {
 
-    }
 
+    }
 
     // 기능
-    public int calculate(int num1, int num2, char cs) {
-
-        int result = 0;
-        if (cs == '+') {
-            result = num1 + num2;
-        } else if (cs == '-') {
-            result = num1 - num2;
-        } else if (cs == '*') {
-            result = num1 * num2;
-        } else if (cs == '/') {
-            if (num2 != 0) {
-                result = num1 / num2;
-            } else {
-                System.out.println("0으로 나눌 수 없습니다.");
-                return 0;
-            }
-        } else {
-            System.out.println("잘못된 연산자입니다.");
-            return 0;
+    public Integer calculate(int num1, int num2, char operator) {
+        if (num1 < 0 || num2 < 0) {
+            System.out.println("양의 정수를 입력하세요.");
+            return null;
         }
+
+        int result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    return null;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("잘못된 연산자입니다.");
+                return null;
+        }
+
         resultList.add(result);
         return result;
-
     }
-
 
 
     // getter
     public List<Integer> getResultList() {
         return resultList;
     }
+
+
     // setter
     public void setResultList(List<Integer> resultList) {
         this.resultList = resultList;
     }
 
+    public void removeResult() {
+        if (resultList.size() > 1) {
+            this.resultList.remove(0);
+        }
+    }
+
 
 }
+
