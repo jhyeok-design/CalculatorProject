3099e08e55def8d4b923a9f6d8488dc03d116ded
최정혁 <jeonghyeogc01@gmaile.com>
2025-10-17 19:11:26 +0900

    Main LV2 런타임때 문자입력시 오류가 나오는 부분 수정

--- src/calculatorpj/Main.java
+++ src/calculatorpj/Main.java
@@ -1,5 +1,6 @@
 package calculatorpj;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -8,35 +9,46 @@ public class Main {
         Calculator calculator = new Calculator();
 
         while (true) {
-            System.out.println("첫번째 정수를 입력하세요.");
-            int num1 = scanner.nextInt();
-
-            System.out.println("사칙연산 기호를 입력하세요.");
-            char cs = scanner.next().charAt(0);
-
-            System.out.println("두번째 정수를 입력하세요.");
-            int num2 = scanner.nextInt();
-
-            if (num1 < 0 || num2 < 0){
-                System.out.println("잘못된 연산자입니다.");
+            int num1 = 0;
+            try {
+                System.out.println("첫번째 정수를 입력하세요.");
+                num1 = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("숫자를 입력하세요.");
+                scanner.nextLine();
                 continue;
             }
 
-            int result = calculator.calculate(num1, num2, cs);
-            System.out.println("결과 값: " + result);
+            System.out.println("사칙연산 기호를 입력하세요.");
+            char operator = scanner.next().charAt(0);
+
+
+            int num2 = 0;
+            try {
+                System.out.println("두번째 정수를 입력하세요.");
+                num2 = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("숫자를 입력하세요.");
+                scanner.nextLine();
+                continue;
+            }
+            Integer result = calculator.calculate(num1, num2, operator);
+            if (result != null) {
+                System.out.println("결과 값 : " + result);
+                System.out.println(calculator.getResultList());
+                calculator.removeResult();
+            }
+
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String answer = scanner.next();
-            if(answer.equals("exit")) {
+            if (answer.equals("exit")) {
                 break;
             }
-
-            // getter
-            System.out.println(calculator.getResultList());
-
 
 
         }
     }
 }
+
 
